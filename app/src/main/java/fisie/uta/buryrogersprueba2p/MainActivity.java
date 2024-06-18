0cb7395a4faa3a6d6ad3e9746ee15d015f331d86
Rogers Bury <rogersbury@gmail.com>
2024-06-18 17:31:32 -0500

    MetodosMainActivity

--- app/src/main/java/fisie/uta/buryrogersprueba2p/MainActivity.java
+++ app/src/main/java/fisie/uta/buryrogersprueba2p/MainActivity.java
@@ -1,6 +1,10 @@
 package fisie.uta.buryrogersprueba2p;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText nombreET;
+    private EditText apellidoET;
+    private EditText dividendoET;
+    private EditText divisorET;
+    private EditText parEnteraET;
+    private EditText residuoET;
+    private EditText numInvertidoET;
+    private Button mostrarBttn;
+    private Button siguienteBttn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +34,44 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        nombreET = findViewById(R.id.editTextNombreAct1);
+        apellidoET = findViewById(R.id.editTextApellidoAct1);
+        dividendoET = findViewById(R.id.editTextDividendoAct1);
+        divisorET = findViewById(R.id.editTextDivisorAct1);
+        parEnteraET = findViewById(R.id.editTextParteEnteAct1);
+        residuoET = findViewById(R.id.editTextResiduoAct1);
+        numInvertidoET = findViewById(R.id.editTextNumInveAct1);
+        siguienteBttn = findViewById(R.id.buttonsiguienteAct1);
+        mostrarBttn = findViewById(R.id.buttonMostrarResAct1);
 
+        desactivarEditText();
+
+        siguienteBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        MainActivity.this, ActivityDos.class);
+
+                String obetenerNombre = nombreET.getText().toString();
+                String obtenerApellido = apellidoET.getText().toString();
+
+                intent.putExtra("keyName", obetenerNombre);
+                intent.putExtra("keyLastName", obtenerApellido);
+
+                startActivity(intent);
+            }
+        });
 
     }
+
+    public void desactivarEditText(){
+        nombreET.setEnabled(false);
+        apellidoET.setEnabled(false);
+        dividendoET.setEnabled(false);
+        divisorET.setEnabled(false);
+        numInvertidoET.setEnabled(false);
+        parEnteraET.setEnabled(false);
+        residuoET.setEnabled(false);
+    }
+
 }
